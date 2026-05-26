d8789eea061ef237be586e99ceb6f253cac55d98
Andy Maloney <amaloney@mailbox.org>
2026-05-26 15:38:37 -0500

    adding debug statements

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,10 @@ cd $SRC_DIR/enable
 # * --no-build-isolation tells `pip` to trust the `conda` environment's tools
 # * [layout,svg] extras are required for chaco
 "${PYTHON}" -m pip install .[layout,svg] --no-build-isolation --no-deps --ignore-installed -v --prefix="${PREFIX}"
+echo "Files installed"
+"${PYTHON}" -c "import site; print(site.getsitepackages())"
+"${PYTHON}" -c "import enable; from pathlib import Path; print(Path(enable.__file__).resolve())"
+ls "${SP_DIR}/enable" 2>/dev/null || echo "NOT in SP_DIR"
 
 # verify `enable` installed correctly
 if [[ ! -f "${SP_DIR}/enable/__init__.py" ]]; then
@@ -24,6 +28,10 @@ fi
 # ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 cd $SRC_DIR/chaco
 "${PYTHON}" -m pip install . --no-build-isolation --no-deps --ignore-installed -v --prefix="${PREFIX}"
+echo "Files installed"
+"${PYTHON}" -c "import site; print(site.getsitepackages())"
+"${PYTHON}" -c "import chaco; from pathlib import Path; print(Path(chaco.__file__).resolve())"
+ls "${SP_DIR}/chaco" 2>/dev/null || echo "NOT in SP_DIR"
 
 # verify `chaco` installed correctly
 if [[ ! -f "${SP_DIR}/chaco/__init__.py" ]]; then
