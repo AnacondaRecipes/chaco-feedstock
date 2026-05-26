38317d1fd05a107ffdbdf2b617bbe5432d538200
Andy Maloney <amaloney@mailbox.org>
2026-05-26 15:59:02 -0500

    python errors

--- recipe/build.bat
+++ recipe/build.bat
@@ -15,20 +15,11 @@ if errorlevel 1 exit /b 1
 "%PYTHON%" -m pip install ".[layout,svg]" --no-build-isolation --no-deps --ignore-installed -v --prefix="%PREFIX%"
 if errorlevel 1 exit /b 1
 
-echo Files installed by enable
-"%PYTHON%" -c "import site; print(site.getsitepackages())"
-dir "%SP_DIR%\enable" 2>nul || echo "NOT in SP_DIR"
-"%PYTHON%" -c "import enable; from pathlib import Path; print(Path(enable.__file__).resolve())"
-
 :: verify `enable` installed correctly
 if not exist "%SP_DIR%\enable\__init__.py" (
     echo ERROR: enable\__init__.py not found in %SP_DIR%
     exit /b 1
 )
-
-:: ensure `enable` can be imported correctly
-"%PYTHON%" -c "import enable; print('enable installed')"
-if errorlevel 1 exit /b 1
 
 :: ─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
 :: `chaco`
@@ -39,18 +30,10 @@ if errorlevel 1 exit /b 1
 "%PYTHON%" -m pip install . --no-build-isolation --no-deps --ignore-installed -v --prefix="%PREFIX%"
 if errorlevel 1 exit /b 1
 
-echo Files installed by chaco
-dir "%SP_DIR%\chaco" 2>nul || echo "NOT in SP_DIR"
-"%PYTHON%" -c "import chaco; from pathlib import Path; print(Path(chaco.__file__).resolve())"
-
 :: verify `chaco` installed correctly
 if not exist "%SP_DIR%\chaco\__init__.py" (
     echo ERROR: chaco\__init__.py not found in %SP_DIR%
     exit /b 1
 )
-
-:: ensure `chaco` can be imported correctly
-"%PYTHON%" -c "import chaco; print('chaco installed')"
-if errorlevel 1 exit /b 1
 
 exit /b 0
