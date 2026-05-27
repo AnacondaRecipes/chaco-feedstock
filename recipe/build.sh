293e30cb13cf50674f30b3b9c96c47451c7b591c
Andy Maloney <amaloney@mailbox.org>
2026-05-27 12:15:00 -0500

    bug fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,21 +6,21 @@ cd $SRC_DIR/enable
 
 # * --no-build-isolation tells `pip` to trust the `conda` environment's tools
 # * [layout,svg] extras are required for chaco
-"${PYTHON}" -m pip install .[layout,svg] --no-build-isolation --no-deps --ignore-installed -v --prefix="${PREFIX}"
+"$PYTHON" -m pip install .[layout,svg] --no-build-isolation --no-deps --ignore-installed -v --prefix="$PREFIX"
 
 # verify `enable` installed correctly
-if [[ ! -f "${SP_DIR}/enable/__init__.py" ]]; then
-    echo "ERROR: enable/__init__.py not found in ${SP_DIR}" >&2
+if [[ ! -f "$SP_DIR/enable/__init__.py" ]]; then
+    echo "ERROR: enable/__init__.py not found in $SP_DIR" >&2
     exit 1
 fi
 
 # `chaco`
 cd $SRC_DIR/chaco
 
-"${PYTHON}" -m pip install . --no-build-isolation --no-deps --ignore-installed -v --prefix="${PREFIX}"
+"$PYTHON" -m pip install . --no-build-isolation --no-deps --ignore-installed -v --prefix="$PREFIX"
 
 # verify `chaco` installed correctly
-if [[ ! -f "${SP_DIR}/chaco/__init__.py" ]]; then
-    echo "ERROR: enable/__init__.py not found in ${SP_DIR}" >&2
+if [[ ! -f "$SP_DIR/chaco/__init__.py" ]]; then
+    echo "ERROR: enable/__init__.py not found in $SP_DIR" >&2
     exit 1
 fi
